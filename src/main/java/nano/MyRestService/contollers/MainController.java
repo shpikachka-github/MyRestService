05c79437620dba6cb9275c03143f585ed4f6c6a8
nano <shpikachka.p@gmail.com>
2020-10-01 14:10:24 +0400

    update ui

--- src/main/java/nano/MyRestService/contollers/MainController.java
+++ src/main/java/nano/MyRestService/contollers/MainController.java
@@ -1,25 +1,29 @@
 package nano.MyRestService.contollers;
 
 import nano.MyRestService.model.Student;
+import nano.MyRestService.service.StudentService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MainController {
 
-    private static List<Student> students = new ArrayList<>();
+    private final StudentService studentService;
 
     @Value("${welcome.message}")
     private String message;
 
     @Value("${error.message}")
     private String errorMessage;
+
+    public MainController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
     public String index(Model model) {
@@ -29,8 +33,9 @@ public class MainController {
         return "index";
     }
 
-    @RequestMapping(value = { "/studentsList" }, method = RequestMethod.GET)
+    @RequestMapping(value = {"/studentsList"}, method = RequestMethod.GET)
     public String studentsList(Model model) {
+        final List<Student> students = studentService.readAll();
 
         model.addAttribute("students", students);
 
