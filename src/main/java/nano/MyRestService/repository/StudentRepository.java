44c1f0bad4cd16815f2a0f27d33f156cfc8a8dda
nano <shpikachka.p@gmail.com>
2020-10-12 02:24:48 +0400

    remove spring-data-jpa

--- src/main/java/nano/MyRestService/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package nano.MyRestService.repository;
-
-import nano.MyRestService.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
