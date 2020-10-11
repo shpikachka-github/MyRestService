10db11d82ffe233645625b62a3eeb15e5449e4bc
nano <shpikachka.p@gmail.com>
2020-10-12 00:23:16 +0400

    update Application

--- src/main/java/nano/MyRestService/Application.java
+++ src/main/java/nano/MyRestService/Application.java
@@ -1,13 +1,7 @@
 package nano.MyRestService;
 
-import nano.MyRestService.model.Student;
-import nano.MyRestService.repository.StudentRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
@@ -15,5 +9,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
