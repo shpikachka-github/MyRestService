bd28ab591d5ee1afec8ddb8ae8cc3f5164fb82b3
nano <shpikachka.p@gmail.com>
2020-10-12 02:18:11 +0400

    update StudentRepository

--- src/main/java/nano/MyRestService/repository/StudentRepository.java
+++ src/main/java/nano/MyRestService/repository/StudentRepository.java
@@ -1,10 +1,21 @@
 package nano.MyRestService.repository;
 
 import nano.MyRestService.model.Student;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
+    @Query("select * from students")
+    List<Student> findAllStudents();
+
+    @Modifying
+    @Query("delete from students where id = :id")
+    void myDeleteById(@Param("id") Long id);
 }
